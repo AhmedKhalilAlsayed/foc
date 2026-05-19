651e1ff56302a9a71e1d0d3e0cac8be115d2117e
AhmedKhalilAlsayed <ahmedkhalilalsayed@gmail.com>
2026-05-19 15:31:34 +0300

    adding the structure and the control block diagram

--- foc_logic/foc_control.cpp
+++ foc_logic/foc_control.cpp
@@ -1,3 +1,30 @@
 class FOCControl
 {
+private:
+	/* data */
+public:
+	FOCControl(/* args */);
+	void run();
+	~FOCControl();
 };
+
+FOCControl::FOCControl(/* args */)
+{
+}
+
+FOCControl::~FOCControl()
+{
+}
+
+void FOCControl::run()
+{
+	/*PI controller*/
+
+	/*IPark, IClarke*/
+
+	/*PWMs*/
+
+	/*Clarke, Park*/
+
+	/*low pass-filter*/
+}
